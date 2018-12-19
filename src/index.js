27b81fd25e7f5b5f9ddad2cd74c0c337703030ca
minjun kim <rlaalswns523@gmail.com>
2018-12-19 14:34:40 +0900

    section 5-7

--- src/index.js
+++ src/index.js
@@ -5,6 +5,23 @@ import './index.css';
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
+import { createStore } from 'redux';
+import reducers from './reducers';
+import * as actions from './actions';
+
+const store = createStore(reducers);
+
+console.log(store.getState());
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+store.dispatch(actions.increment());
+store.dispatch(actions.increment());
+store.dispatch(actions.decrement());
+store.dispatch(actions.setColor([200, 200, 200]));
+
+unsubscribe();
+
+store.dispatch(actions.setColor([210, 210, 210]));
+
 ReactDOM.render(
   <App></App>,
   document.getElementById("root")
